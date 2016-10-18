e34639876ad5f1e6e2ecfd292ac57c49c5733fe2
James Lee and Maxwell Cady <maxwell@>
2016-10-18 11:59:57 -0700

    Add jQuery to make flashcards and added styling

---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+$(function() {
+
+  $("#javascript").click(function() {
+    $("#javascript>.description").toggle();
+    $("#javascript>.title").toggle();
+  })
+
+  $("#operators").click(function() {
+    $("#operators>.description").toggle();
+    $("#operators>.title").toggle();
+  })
+
+  $("#variables").click(function() {
+    $("#variables>.description").toggle();
+    $("#variables>.title").toggle();
+  })
+
+  $("#variableNaming").click(function() {
+    $("#variableNaming>.description").toggle();
+    $("#variableNaming>.title").toggle();
+  })
+
+  $("#functions").click(function() {
+    $("#functions>.description").toggle();
+    $("#functions>.title").toggle();
+  })
+
+  $("#methods").click(function() {
+    $("#methods>.description").toggle();
+    $("#methods>.title").toggle();
+  })
+
+  $("#arguments").click(function() {
+    $("#arguments>.description").toggle();
+    $("#arguments>.title").toggle();
+  })
+
+  $("#parameters").click(function() {
+    $("#parameters>.description").toggle();
+    $("#parameters>.title").toggle();
+  })
+
+  $("#return").click(function() {
+    $("#return>.description").toggle();
+    $("#return>.title").toggle();
+  })
+
+  $("#chaining").click(function() {
+    $("#chaining>.description").toggle();
+    $("#chaining>.title").toggle();
+  })
+
+  $("#strings").click(function() {
+    $("#strings>.description").toggle();
+    $("#strings>.title").toggle();
+  })
+
+  $("#booleans").click(function() {
+    $("#booleans>.description").toggle();
+    $("#booleans>.title").toggle();
+  })
+
+  $("#undefined").click(function() {
+    $("#undefined>.description").toggle();
+    $("#undefined>.title").toggle();
+  })
+
+  $("#nan").click(function() {
+    $("#nan>.description").toggle();
+    $("#nan>.title").toggle();
+  })
+
+  $("#escape").click(function() {
+    $("#escape>.description").toggle();
+    $("#escape>.title").toggle();
+  })
+
+  $("#alert").click(function() {
+    $("#alert>.description").toggle();
+    $("#alert>.title").toggle();
+  })
+
+  $("#comments").click(function() {
+    $("#comments>.description").toggle();
+    $("#comments>.title").toggle();
+  })
+
+  $("#jquery").click(function() {
+    $("#jquery>.description").toggle();
+    $("#jquery>.title").toggle();
+  })
+
+  $("#attributes").click(function() {
+    $("#attributes>.description").toggle();
+    $("#attributes>.title").toggle();
+  })
+})
